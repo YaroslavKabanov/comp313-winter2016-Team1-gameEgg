090823b84e4f85ee344fd13c3c3a5b4a2335a0d3
Nikita Chernykh <chernykhmain@gmail.com>
2016-02-24 17:29:57 -0500

    Menu added and Level Geneirator

---
+++ Assets/Scripts/Seed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seed : MonoBehaviour {
+	private static int pointsToAdd = 1;
+	public AudioClip soundFile;
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player") {
+			ScoreManager.AddPoints (pointsToAdd);
+			Destroy(gameObject);
+		}  
+
+	}
+	void Start () {
+		
+
+	}
+}
